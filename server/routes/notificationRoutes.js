9710baeb75cc2033b0dae6f96f93ec2a4ce35f35
vuminh2005 <vuminhnguyen25@gmail.com>
2025-12-21 15:46:52 +0700

    add button check all noti

--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUserNotifications, markAsRead } from "../controllers/notificationController.js";
+import { getUserNotifications, markAsRead, markAllRead } from "../controllers/notificationController.js";
 
 const router = express.Router();
 
 router.get("/:userId", getUserNotifications); // Lấy thông báo
 router.put("/:notificationId/read", markAsRead); // Đánh dấu đã đọc
+router.put("/:userId/read-all", markAllRead); // Đánh dấu tất cả đã đọc cho user
 
 export default router;
